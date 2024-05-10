05e182a8a6991b3f4e5c9d09a37fa87082a6e6bb
Hila Gvar <hilla.gvar@gmail.com>
2024-05-10 06:57:50 +0200

    edit

--- src/app/directives/phone-validator.directive.ts
+++ src/app/directives/phone-validator.directive.ts
@@ -18,12 +18,12 @@ export class PhoneValidatorDirective implements Validator {
   validate(control: AbstractControl<any, any>): ValidationErrors | null {
     const phoneNumber : string = control.value;
 
-    if (control.value != null ) {
+    if (control.value !== null ) {
       if (phoneNumber[0] !== "+") {
         return {error: "Priekyje nera pliuso"};
       } else if (phoneNumber.length <10 || phoneNumber.length >12) {
         return {error: "Netinkamas ilgis"};
-      } else if (isNaN(Number(phoneNumber.slice(1,phoneNumber.length)))) {
+      } else if (isNaN(Number(phoneNumber.slice(1)))) {
         return {error: "Netinkami simboliai"};
       } else
       return null;
